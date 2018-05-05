4d9f1091f197f75ced25d46ae3773afd0eba8c4b
Janani <janani@github.com>
2018-05-05 08:56:59 -0400

    Added a comment

---
+++ src/main/java/com/in28minutes/springbootin10steps/com/in28minutes/springbootin10steps/Book.java
@@ -0,0 +1,32 @@
+package com.in28minutes.springbootin10steps.com.in28minutes.springbootin10steps;
+
+public class Book {
+	
+	int id;
+	String name;
+	String author;
+	
+	public Book(int id, String name, String author) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+	}
+		
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Book [id=%s, name=%s, author=%s]", id, name, author);
+	}
+	//trying another commit
+
+}
